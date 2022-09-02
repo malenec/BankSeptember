72542709776ea3dbc9b7b66dd59c52e3cbc6f9f5
Israa El-Haj Moussa <din-israa@hotmail.com>
2022-09-02 10:38:43 +0200

    fixes bankTest variables

--- src/BankTest.java
+++ src/BankTest.java
@@ -3,14 +3,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankTest {
-
     Bank bank = new Bank();
-
 
     @Test
     void createAccount() {
-        assertEquals(true, bank.CreateAccount(new Account("Mark")));
-        assertEquals(false, bank.CreateAccount(new Account("Mark")));
-
+        assertEquals(true, bank.createAccount(new Account("Mark")));
+        assertEquals(false, bank.createAccount(new Account("Mark")));
     }
 }
