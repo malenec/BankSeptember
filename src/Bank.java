19ccca7281a87cb6325c2d46640ea98de00e704d
Israa El-Haj Moussa <din-israa@hotmail.com>
2022-09-02 10:35:49 +0200

    fixes bank variables

--- src/Bank.java
+++ src/Bank.java
@@ -3,15 +3,13 @@ import java.util.TreeMap;
 
 public class Bank {
 
-    private Map<String, Account> kontoMap = new TreeMap<>();
-
+    private Map<String, Account> accountMap = new TreeMap<>();
     public boolean createAccount(Account account) {
-        if (!kontoMap.containsKey(account.getName())) {
-            kontoMap.put(konto.getName(), account);
+        if (!accountMap.containsKey(account.getName())) {
+            accountMap.put(account.getName(), account);
             return true;
         }
         return false;
     }
-
 }
 
